db05f9f2630b51c0f8d80cf8905ac07d84a75c18
DannOut <danielxdn@gmail.com>
2023-02-13 15:36:22 -0300

    feat: criando base Car do projeto

--- src/Domains/Car/Car.ts
+++ src/Domains/Car/Car.ts
@@ -2,9 +2,35 @@ import ICar from '../../Interfaces/ICar';
 import AbstractAuto from '../Vehicle/Vehicle';
 
 export default class Car extends AbstractAuto {
+  private _doorQty: number;
+  private _seatsQty: number;
+
   constructor({
-    
+    id,
+    model,
+    year,
+    color,
+    status,
+    buyValue,
+    doorQty,
+    seatsQty,
   }: ICar) {
-    super();
+    super({ id, model, year, color, status, buyValue });
+    this._doorQty = doorQty;
+    this._seatsQty = seatsQty;
+  }
+
+  public get seatsQty(): number {
+    return this._seatsQty;
+  }
+  public set seatsQty(value: number) {
+    this._seatsQty = value;
+  }
+
+  public get doorQty(): number {
+    return this._doorQty;
+  }
+  public set doorQty(value: number) {
+    this._doorQty = value;
   }
 }
