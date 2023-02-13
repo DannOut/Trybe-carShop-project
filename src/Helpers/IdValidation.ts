d6119f07d154078ffd30ccbdd5ffff8617b79741
DannOut <danielxdn@gmail.com>
2023-02-13 18:35:32 -0300

    feat: req 2 concluded

--- src/Helpers/IdValidation.ts
+++ src/Helpers/IdValidation.ts
@@ -4,7 +4,7 @@ import ErrorHandler from './ErrorHandler';
 const validateId = (req: Request, _res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
-    if (id.length !== 24) throw new ErrorHandler(422, 'Invalid mongo id');
+    if (id.length % 24 !== 0) throw new ErrorHandler(422, 'Invalid mongo id');
     next();
   } catch (e) {
     next(e);
