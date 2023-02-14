46d02568560fddf7a759af6fd9dd03e45940ea14
DannOut <danielxdn@gmail.com>
2023-02-14 15:55:45 -0300

    feat: advancing refactoring

--- src/Controllers/Abstract.Controller.ts
+++ src/Controllers/Abstract.Controller.ts
@@ -1,15 +1,72 @@
-// import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import AbstractService from '../Services/Abstract.Service';
 
-// export default abstract class AbstractController<T> {
-//   protected _req: Request;
-//   protected _res: Response;
-//   protected next: NextFunction;
-//   protected _service: AbstractService<T>
+export default abstract class AbstractController<T> {
+  protected _req: Request;
+  protected _res: Response;
+  protected next: NextFunction;
+  protected _service: AbstractService<T>;
 
-//   constructor(req: Request, res: Response, next: NextFunction, service: AbstractService<T>) {
-//     this._req = req;
-//     this._res = res;
-//     this.next = next;
-//     this._service = service;
-//   }
-// }
+  //  prettier-ignore
+  constructor(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    service: AbstractService<T>,
+  ) {
+    this._req = req;
+    this._res = res;
+    this.next = next;
+    this._service = service;
+  }
+
+  public async findAll() {
+    try {
+      const data = await this._service.findAll();
+      return this._res.status(200).json(data);
+    } catch (e) {
+      this.next(e);
+    }
+  }
+
+  public async findById() {
+    try {
+      const { id } = this._req.params;
+      const data = await this._service.findById(id);
+      return this._res.status(200).json(data);
+    } catch (e) {
+      this.next();
+    }
+  }
+
+  public async create() {
+    try {
+      const newVehicle = this._req.body;
+      const data = await this._service.create(newVehicle);
+      return this._res.status(201).json(data);
+    } catch (e) {
+      this.next(e);
+    }
+  }
+
+  public async update() {
+    try {
+      const { id } = this._req.params;
+      const updatedInfo = this._req.body;
+      const data = await this._service.update(id, updatedInfo);
+      return this._res.status(200).json(data);
+    } catch (e) {
+      this.next(e);
+    }
+  }
+
+  public async remove() {
+    try {
+      const { id } = this._req.params;
+      await this._service.remove(id);
+      return this._res.sendStatus(204);
+    } catch (e) {
+      this.next(e);
+    }
+  }
+}
